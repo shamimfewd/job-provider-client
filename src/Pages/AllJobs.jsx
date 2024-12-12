39bc0c56236881f635b5a8c27727171efc9baf51
shamim <shamimmahmud077@gmail.com>
2024-12-12 23:00:16 +0600

    added magination

--- src/Pages/AllJobs.jsx
+++ src/Pages/AllJobs.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import JobCard from "../components/JobCard";
+import axios from "axios";
 
 const AllJobs = () => {
-  const pages = [1, 2, 3, 4, 5];
+  const [itemPerPage, setItemPerPage] = useState(2);
+  const [count, setCount] = useState(0);
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios("http://localhost:5000/jobs");
+      setJobs(data);
+      setCount(data.length);
+    };
+
+    getData();
+  }, []);
+
+  const pages = [...Array(5).keys()].map((element) => element + 1);
+
+  console.log(jobs);
+  console.log(count);
   return (
     <div className="container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
       <div>
@@ -48,12 +67,13 @@ const AllJobs = () => {
           <button className="btn">Reset</button>
         </div>
         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {/* {jobs.map(job => (
+          {jobs.map((job) => (
             <JobCard key={job._id} job={job} />
-          ))} */}
+          ))}
         </div>
       </div>
 
+      {/* Pagination section */}
       <div className="flex justify-center mt-12">
         <button className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white">
           <div className="flex items-center -mx-1">
